537ec38c944b68dc6e6da5ed1fbc69800251cb00
WP2_Darrel Hutomo <90746275+Darrel208@users.noreply.github.com>
2021-09-21 12:51:16 +0700

    Add files via upload

---
+++ contoh1.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers;
+class contoh1 extends BaseController
+
+{
+ public function index()
+ {
+ echo "<h1>Perkenalkan</h1>";
+ echo"Nama saya Darrel Hutomo
+ Saya tingga di daerah Bekasi
+ Hobi saya bermain game";
+ }
+}
