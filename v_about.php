66761ee208492111e193cf2f6606949ad0ab26bf
WP2_Darrel Hutomo <90746275+Darrel208@users.noreply.github.com>
2021-10-08 17:09:48 +0700

    Add files via upload

---
+++ v_about.php
@@ -0,0 +1,23 @@
+<section>
+ <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Darrel</li>
+        <li>Nama Belakang : Hutomo</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Jalan Raya Hankam Rt 06/05 NO 20</li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Bekasi</li>
+    </ul>
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Bulutangkis</li>
+        <li>Berenang</li>
+    </ul>
+</section>
